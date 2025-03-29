d78f97d1702abec1dc16391c204afcfe0619f949
Evgeni <qazplm2125a@gmail.com>
2025-03-29 16:58:00 +0500

    m5 t1

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -4,13 +4,13 @@ import { isFuturePoint, isPastPoint, isPresentPoint } from './utls';
 const filter = {
   [filterType.EVERYTHING]: (points)=> points,
   [filterType.FUTURE]: (points)=> points.filter((point)=>
-    isFuturePoint(point.date_from)
+    isFuturePoint(point.dateFrom)
   ),
   [filterType.PRESENT]: (points)=> points.filter((point)=>
-    isPresentPoint(point.date_from, point.date_to)
+    isPresentPoint(point.dateFrom, point.dateTo)
   ),
   [filterType.PAST]: (points)=> points.filter((point)=>
-    isPastPoint(point.date_to)
+    isPastPoint(point.dateTo)
   )
 };
 
